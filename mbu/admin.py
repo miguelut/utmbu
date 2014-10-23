6783696210649cb9b493fbc9263f67cf698492dd
Bryant Pham <bryantphamm@gmail.com>
2014-10-22 19:30:54 -0500

    Added admin page

--- mbu/admin.py
+++ mbu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mbu.models import Course, Session, Scout, Troop, TroopContact, Address, Counselor, Venture, MeritBadgeUniversity, WaitingList
+
+admin.site.register(Course)
+admin.site.register(Session)
+admin.site.register(Scout)
+admin.site.register(Troop)
+admin.site.register(TroopContact)
+admin.site.register(Address)
+admin.site.register(Counselor)
+admin.site.register(Venture)
+admin.site.register(MeritBadgeUniversity)
+admin.site.register(WaitingList)
 
 # Register your models here.
