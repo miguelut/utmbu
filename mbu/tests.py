f9f904a5a4da818eb5e948f2df0e5083661a5143
Bryant Pham <bryantphamm@gmail.com>
2015-01-13 19:22:37 -0600

    Added home page test

--- mbu/tests.py
+++ mbu/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomePageTests(TestCase):
+	def test_home_page(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
