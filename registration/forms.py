5a6aefc7b1e7f5fe82133814bf96a6b41e90ce6b
Michael Wright <michael.wright@parivedasolutions.com>
2015-01-30 15:23:03 -0600

    Adding base for scout registration form.

--- registration/forms.py
+++ registration/forms.py
@@ -1,7 +1,3 @@
 from django.forms import ModelForm
 from scout.models import Scout
 
-class ScoutForm(ModelForm):
-    class Meta:
-        model = Scout
-        fields = ['User.first_name']
--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, render_to_response
-from registration.forms import ScoutForm
+from django.core.context_processors import csrf
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from scout.models import Scout
+from django.forms.models import inlineformset_factory
 
 # Create your views here.
 
 def register_scout(request):
+    if request.POST:
+        pass
     args = {}
-    args.update({'form': ScoutForm() })    
+    ScoutFormSet = inlineformset_factory(User, Scout, can_delete=False)
+    user = User()
+    form = UserCreationForm()
+    formset = ScoutFormSet(instance=user)
+    args.update(csrf(request))
+    args.update({'form' : form })
+    args.update({'formset': formset })    
     return render_to_response('registration/register_scout.html', args)
 
 def register_scoutmaster(request):
