eb04a82bccd894991cb680b67a4d2420655225d2
Michael Wright <michael.wright@parivedasolutions.com>
2015-01-31 07:51:54 -0600

    Adding messages and redirect to scout creation.

--- registration/forms.py
+++ registration/forms.py
@@ -1,3 +1,6 @@
-from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.forms.models import inlineformset_factory
 from scout.models import Scout
 
+
+ScoutFormSet = inlineformset_factory(User, Scout, can_delete=False)
--- registration/views.py
+++ registration/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render, render_to_response
+from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from scout.models import Scout
-from django.forms.models import inlineformset_factory
+from django.contrib import messages
+from registration.forms import ScoutFormSet
 
 # Create your views here.
 
 def register_scout(request):
-    if request.POST:
-        pass
     args = {}
-    ScoutFormSet = inlineformset_factory(User, Scout, can_delete=False)
     user = User()
     form = UserCreationForm()
-    formset = ScoutFormSet(instance=user)
+    formset = ScoutFormSet()
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            scout_formset = ScoutFormSet(request.POST, instance=user)
+            if scout_formset.is_valid():
+                user.save()
+                scout_formset.save()
+                messages.add_message(request, messages.SUCCESS, 'Registration complete.  Please log in.')
+                return HttpResponseRedirect('/login')                
+
     args.update(csrf(request))
     args.update({'form' : form })
     args.update({'formset': formset })    
