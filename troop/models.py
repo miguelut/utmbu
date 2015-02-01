9f2a5506eb6024b18195c8b514a4ade63a7d8f96
Michael Wright <michael.wright@parivedasolutions.com>
2015-01-31 20:13:35 -0600

    Adding support for first_name and last_name on scout creation form.  Adding date selection widget.

--- troop/models.py
+++ troop/models.py
@@ -7,13 +7,23 @@ from mbu.models import Address
 class Council(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Troop(models.Model):
     number = models.CharField(max_length=10)
     council = models.ForeignKey(Council)
+
+    def __str__(self):
+        return "%s - %s" % (self.number, self.council)
+
 
 class TroopContact(models.Model):
     user = models.OneToOneField(User)
     address = models.ForeignKey(Address)
     troop = models.ForeignKey(Troop)
     phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return "%s %s %s" % (self.user.first_name, self.user.last_name, self.phone_number)
 
