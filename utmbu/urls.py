e8d8240705f6a54fabea93f7773aa71dc9881235
Bryant Pham <bryantphamm@gmail.com>
2015-04-20 19:38:54 -0500

    Added functionality to add and remove scout classes as scoutmaster

--- utmbu/urls.py
+++ utmbu/urls.py
@@ -42,7 +42,7 @@ urlpatterns += patterns(
 urlpatterns += patterns(
     '',
     url(r'^scoutmaster/viewtroop/$', 'scoutmaster.views.view_troop_enrollees', name ='scoutmaster_view_troop'),
-    url(r'^scoutmaster/viewclasses/(?P<scout_id>\d)/$', 'scoutmaster.views.view_troop_classes', name ='scoutmaster_view_classes')
+    url(r'^scoutmaster/editclasses/(?P<scout_id>\d)/$', 'scoutmaster.views.edit_troop_classes', name ='scoutmaster_edit_classes')
 )
 
 #Dev URLs -- REMOVE BEFORE DEPLOYMENT
