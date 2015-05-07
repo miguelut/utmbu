be9362aa7dd2e1dcb8f40795dec0c02be19c4546
Michael M. Wright <michael.wright@parivedasolutions.com>
2015-05-06 22:55:03 -0500

    Updating tests to support new login paradigm.

--- mbu/tests.py
+++ mbu/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
 class HomePageTests(TestCase):
-	def test_home_page(self):
+	def test_home_page_redirect_on_logged_out(self):
 		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.status_code, 302)
