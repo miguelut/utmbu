7c2def3b9b21756c4882c83facb75f4ea1a96daa
Michael Wright <michael.wright@parivedasolutions.com>
2015-07-21 20:49:27 -0500

    Adding permission to scouts created with social auth.

--- mbu/pipeline/user_type.py
+++ mbu/pipeline/user_type.py
@@ -1,4 +1,5 @@
 from mbu.models import Scout
+from django.contrib.auth.models import Permission, ContentType
 
 def user_get_names(backend, details, response, is_new=False, *args, **kwargs):
     data = backend.strategy.request_data();
@@ -10,6 +11,13 @@ def user_get_names(backend, details, response, is_new=False, *args, **kwargs):
 
 def user_make_scout(backend, details, response, is_new=False, *args, **kwargs):
     user = kwargs.get('user')
-    scout = Scout.objects.get(user=user)
+    try:
+        scout = Scout.objects.get(user=user.id)
+    except:
+        scout = None
     if(scout is None):
         Scout(user=user).save()
+        ct = ContentType.objects.get(app_label='mbu', model='scout')
+        perm = Permission.objects.get(codename='edit_scout_schedule', content_type=ct)
+        user.user_permissions.add(perm)
+        user.save()
