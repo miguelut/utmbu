0fd1369205f3f83f096408a522ef9a9ce6264e7d
Bryant Pham <bryantphamm@gmail.com>
2015-09-05 21:23:09 -0500

    Added some sweet ass snapping to the sweet ass drag and drop zones

--- static/js/sweet-ass-drag-n-drop.js
+++ static/js/sweet-ass-drag-n-drop.js
@@ -1,14 +1,31 @@
-var DragonDrop = {
+var SweetAssDragNDrop = {
     init: function() {
         $(".draggable-course").draggable({
             revert: "invalid"
         });
-        $(".course-dropzones").droppable({
+
+        $("#course-catalog-dropzone").droppable({
             drop: function(event, ui) {
-                console.log("HELL YEAH DROPPED ON");
+                var droppedCourse = ui.draggable;
+                var appendingList = $("#catalog-course-list");
+                handleDrop(droppedCourse, appendingList);
             }
         });
+
+        $("#added-course-dropzone").droppable({
+            drop: function(event, ui) {
+                var droppedCourse = ui.draggable;
+                var appendingList = $("#added-course-list");
+                handleDrop(droppedCourse, appendingList);
+            }
+        });
+
+        function handleDrop(droppedCourse, list) {
+            droppedCourse.detach()
+                .css({right: 'auto', left: 'auto', top: 'auto'})
+                .appendTo(list);
+        }
     }
 };
 
-DragonDrop.init();
+SweetAssDragNDrop.init();
