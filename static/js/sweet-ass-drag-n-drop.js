0c8d34e3913fd32da6e34517583ffa859ff68311
Michael M. Wright <michael.wright@parivedasolutions.com>
2016-01-05 21:51:10 -0600

    Combining current class registration look and feel with drag n' drop; removing some functionality and replacing with foundation.

--- static/js/sweet-ass-drag-n-drop.js
+++ static/js/sweet-ass-drag-n-drop.js
@@ -1,48 +1,17 @@
 var SweetAssDragNDrop = {
     init: function() {
-        $(".draggable-course").draggable({
-            revert: 'invalid',
+        $(".class-dragable").draggable({
             appendTo: 'body',
+            revert: 'invalid',
+            containment: '.schedule',
             helper: 'clone'
         });
 
-        $("#course-catalog-dropzone").droppable({
-            drop: function(event, ui) {
-                var droppedCourse = ui.draggable;
-                var appendingList = $("#catalog-course-list");
-                $.post(
-                    "http://localhost:8000/scout/unenroll_course/",
-                    {'course_instance_id': droppedCourse.attr('id')},
-                    function(data) {
-                        handleDrop(droppedCourse, appendingList);
-                    }
-                ).error(function() {
-                    ui.draggable.draggable('option', 'revert', true);
-                });
+        $(".class-dropable").droppable({
+            drop: function(event, ui){
+                this.appendChild(ui.draggable[0]);
             }
         });
-
-        $("#added-course-dropzone").droppable({
-            drop: function(event, ui) {
-                var droppedCourse = ui.draggable;
-                var appendingList = $("#added-course-list");
-                $.post(
-                    "http://localhost:8000/scout/enroll_course/",
-                    {'course_instance_id': droppedCourse.attr('id')},
-                    function(data) {
-                        handleDrop(droppedCourse, appendingList);
-                    }
-                ).error(function() {
-                    ui.draggable.draggable('option', 'revert', true);
-                });
-            }
-        });
-
-        function handleDrop(droppedCourse, list) {
-            droppedCourse.detach()
-                .css({right: 'auto', left: 'auto', top: 'auto'})
-                .appendTo(list);
-        }
     }
 };
 
